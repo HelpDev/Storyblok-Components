faa7f135535bf3a61f1f6facb580febcbb2c3a2d
Quique Fdez Guerra <quique@ckgrafico.com>
2022-05-08 20:53:54 +0300

    Fix class

--- components/page/PageContainer.vue
+++ components/page/PageContainer.vue
@@ -1,10 +1,11 @@
 <template>
-  <div :class="`w-${width}`">
+  <div>
     <component
       v-for="blok in blok.body"
       :key="blok._uid"
       :blok="blok"
       :is="blok.component"
+      :class="`w-${width}`"
     />
   </div>
 </template>
