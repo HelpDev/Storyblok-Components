db80bc20452c4c456743e2bfef22d00d933003fa
Quique Fdez Guerra <quique@ckgrafico.com>
2022-05-08 20:42:13 +0300

    add page container

---
+++ components/page/PageContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="`w-${width}`">
+    <component
+      v-for="blok in blok.body"
+      :key="blok._uid"
+      :blok="blok"
+      :is="blok.component"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    width() {
+      return `h${this.blok.width}`;
+    }
+  }
+};
+</script>
