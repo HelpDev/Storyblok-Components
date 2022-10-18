225ca9061bec2dc5668c93c613e7c4cb09559c7c
Quique Fdez Guerra <quique@ckgrafico.com>
2022-10-18 15:01:55 +0300

    add new styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/styles/main.css'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
